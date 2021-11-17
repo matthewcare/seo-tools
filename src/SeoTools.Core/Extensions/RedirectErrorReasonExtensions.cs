e07febf3981f676bd1900301f72f2a7b48c46c86
Matthew Care <matty_care@hotmail.co.uk>
2021-11-17 16:15:51 +0000

    Redirect error reasons
    Add reasons for why a redirect line might be invalid

---
+++ src/SeoTools.Core/Extensions/RedirectErrorReasonExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using SeoTools.Core.Services.Redirects;
+
+namespace SeoTools.Core.Extensions
+{
+	public static class RedirectErrorReasonExtensions
+	{
+		public static string ToDisplay(this RedirectErrorReason reason)
+		{
+			switch (reason)
+			{
+				case RedirectErrorReason.IncorrectFormat:
+					return "This line is in the incorrect format";
+				case RedirectErrorReason.InvalidFromUri:
+					return "The \"from\" URI is invalid";
+				case RedirectErrorReason.InvalidToUri:
+					return "The \"to\" URI is invalid";
+				case RedirectErrorReason.InvalidFromExtension:
+					return "The \"from\" URI has an invalid extension";
+				case RedirectErrorReason.InvalidToExtension:
+					return "The \"to\" URI has an invalid extension";
+				case RedirectErrorReason.InvalidDestination:
+					return "The destination URI is invalid";
+				case RedirectErrorReason.DuplicateDetected:
+					return "This redirect exists more than once";
+				case RedirectErrorReason.RedirectingToSelf:
+					return "This is redirecting to itself";
+				default:
+					throw new ArgumentOutOfRangeException(nameof(reason), reason, null);
+			}
+		}
+	}
+}
---
+++ src/SeoTools.Core/Services/Redirects/RedirectErrorReason.cs
@@ -0,0 +1,52 @@
+﻿namespace SeoTools.Core.Services.Redirects
+{
+	/// <summary>
+	/// A redirect might not be valid for any of these reasons
+	/// </summary>
+	public enum RedirectErrorReason
+	{
+		/// <summary>
+		/// If the supplied CSV is not in the correct format
+		/// </summary>
+		IncorrectFormat,
+
+		/// <summary>
+		/// If the from URI isn't validated as being a URL
+		/// </summary>
+		InvalidFromUri,
+
+		/// <summary>
+		/// If the to URI isn't validated as being a URL
+		/// </summary>
+		InvalidToUri,
+
+		/// <summary>
+		/// If the from URI has an extension that is not allowed
+		/// </summary>
+		InvalidFromExtension,
+
+		/// <summary>
+		/// If the to URI has an extension that is not allowed
+		/// </summary>
+		InvalidToExtension,
+
+		/// <summary>
+		/// If the to URI is to a destination that is not allowed
+		/// </summary>
+		InvalidDestination,
+
+		/// <summary>
+		/// If there are two redirects with the same from URI
+		/// </summary>
+		DuplicateDetected,
+
+		/// <summary>
+		/// If a redirect is redirecting to itself
+		/// </summary>
+		/// <remarks>
+		///	Seen when trying to redirect /a-url/ to /a-url
+		/// </remarks>
+		RedirectingToSelf
+
+	}
+}
